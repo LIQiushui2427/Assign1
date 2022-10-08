59f0c26ee738901a063056e339e01afb54179c26
unknown <yijian2427@gmail.com>
2022-10-08 16:59:53 +0800

    new file:   Ans.c 	new file:   SimpleShell 	modified:   simple-execute.c 	new file:   simple-execute.o 	new file:   simple-shell.o

---
+++ Ans.c
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+/*	
+	|-----------Group Information-----------|
+		I don't have a group.
+		This file is ZHOU, Yixiang's work.
+		SID: 1155157100
+	|---------------------------------------|
+*/
+
+struct command{
+	char **argv;
+	int argc;
+};
+
+void spawn_command(int in, int out, struct command *cmd){
+	pid_t child_pid;
+
+	if((child_pid = fork()) < 0){
+		perror("fork() error \n");
+	}
+	
+	if (child_pid == 0 ){
+
+		if (in != STDIN_FILENO){
+			// should be replaced with dup2; same for the following
+			close(STDIN_FILENO);
+			dup(in);
+			close(in);
+		}
+
+		if (out != STDOUT_FILENO){
+			close(STDOUT_FILENO);
+			dup(out);
+			close(out);
+		}
+
+		if (execvp(cmd->argv[0], cmd->argv) < 0){
+			perror("execvp() error");
+			exit(1);
+		}
+
+	}
+}
+
+int shell_execute(char ** args, int argc){
+
+	if ( strcmp(args[0], "EXIT") == 0)
+		return -1;
+	
+	int PIPE_FD[2];
+	int INPUT_FD	= STDIN_FILENO;
+	int OUTPUT_FD	= STDOUT_FILENO;
+
+	int head = 0;
+	int tail = 0;
+
+	// head and tail indicate the position of each individual command
+	while (head < argc - 1){
+		while(tail < argc - 1 && (args + tail)[0][0] != '|') tail++;
+		
+		struct command *cmd = malloc(sizeof(struct command));
+		cmd->argc = tail - head;
+		cmd->argv = malloc(sizeof(char *) * (tail - head + 1));
+		for (int i = 0; i < tail - head; i++)
+			cmd->argv[i] = (args + head)[i];
+		cmd->argv[tail - head] = NULL;
+
+		if (tail == argc - 1){
+			// if this is the last command, output to the terminal
+			OUTPUT_FD = STDOUT_FILENO;
+			spawn_command(INPUT_FD, OUTPUT_FD, cmd);
+		} else {
+			// otherwise create a new pipe and redirect the output to write side of the new pipe
+			pipe(PIPE_FD);
+			OUTPUT_FD = PIPE_FD[1];
+			spawn_command(INPUT_FD, OUTPUT_FD, cmd);
+			// remember to close for parent
+			if (INPUT_FD != STDIN_FILENO) close(INPUT_FD);
+			if (OUTPUT_FD != STDOUT_FILENO) close(OUTPUT_FD);
+			// the input of the next command is the read side of the new pipe
+			INPUT_FD = PIPE_FD[0];
+		}
+		
+		free(cmd);
+
+		if (tail >= argc - 1) break;
+		if ((args + tail)[0][0] == '|') head = tail = tail + 1;
+			else break;
+	}
+
+	// Piped commands run concurrently.
+	pid_t wpid;
+	int status = 0;
+	while ((wpid = wait(&status)) > 0); // wait for all the child processes 
+
+	return 0;
+}
--- simple-execute.c
+++ simple-execute.c
@@ -4,26 +4,91 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+// YIJian 1155157207 CSCI3150 Assignment 1
+// This is my own work except the code provided by the instructor.
 
+typedef struct command{
+	char **argv;
+	int argc;
+}command;
+
+void spawn_command(int in, int out, command *cmd){
+	pid_t child_pid;
+
+	if((child_pid = fork()) < 0){
+		perror("fork() error \n");
+	}
+	
+	if (child_pid == 0 ){ // child process
+
+		if (in != STDIN_FILENO){
+			// should be replaced with dup2; same for the following
+			close(STDIN_FILENO);
+			dup(in);
+			close(in);
+		}
+
+		if (out != STDOUT_FILENO){
+			close(STDOUT_FILENO);
+			dup(out);
+			close(out);
+		}
+
+		if (execvp(cmd->argv[0], cmd->argv) < 0){
+			perror("execvp() error");
+			exit(1);
+		}
+
+	}
+}
 int shell_execute(char ** args, int argc)
 {
-	int child_pid, wait_return, status;
-
-	if ( strcmp(args[0], "EXIT") == 0 )
-		return -1; 
+	if(strcmp(args[0], "EXIT") == 0)
+		return -1;
 	
-	if( (child_pid = fork()) < 0 ){
-		printf("fork() error \n");
-	}else if (child_pid == 0 ){
-		if ( execvp(args[0], args) < 0){ 
-			printf("execvp() error \n");
-			exit(-1);
-		}
-	}else{
-		if ( (wait_return = wait(&status) ) < 0 )
-			printf("wait() error \n"); 
-	}
-			
-	return 0;
+	int fd[2];
 
+	int input_fd = 0;
+	int output_fd = 1;
+
+	int head = 0;
+	int tail = 0;
+
+	while(head < argc - 1)
+	{
+		while(tail < argc -1 && (args + tail)[0][0] != '|')
+			tail++;
+		command *cmd = (command *)malloc(sizeof(command));
+		cmd->argc = tail - head;
+		cmd->argv = (char **)malloc(sizeof(char *) * (cmd->argc + 1));
+		for(int i = 0; i < cmd->argc; i++)
+			cmd->argv[i] = args[head + i];
+		cmd->argv[cmd->argc] = NULL;
+
+		if(tail < argc - 1) // not the last command
+		{
+			pipe(fd);
+			output_fd = fd[1];
+			spawn_command(input_fd, output_fd, cmd);
+
+			if(input_fd != 0)
+				close(input_fd);
+			if(output_fd != 1)
+				close(output_fd);
+			input_fd = fd[0];// the output of the previous command is the input of the next command
+		}
+		else if(tail == argc - 1){ // last command
+			output_fd = 1;//STDOUT_FILENO;
+			spawn_command(input_fd, output_fd, cmd);
+		}
+
+		free(cmd);
+		pid_t status = 0;
+		while(wait(&status) != -1);// wait for all the child processes to finish
+		
+		return 0;
+	}
+
+
+	
 }
