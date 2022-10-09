d158d06d68f82e2ad30847d41ac335afe508879a
unknown <yijian2427@gmail.com>
2022-10-09 11:33:51 +0800

    modified:   SimpleShell 	modified:   simple-execute.c 	modified:   simple-execute.o

--- simple-execute.c
+++ simple-execute.c
@@ -5,21 +5,22 @@
 #include <errno.h>
 #include <unistd.h>
 // YIJian 1155157207 CSCI3150 Assignment 1
-// This is my own work except the code provided by the instructor.
+// I am in a group with Juluan SHI
+// Discussed with some friends about the assignment
 
 typedef struct command{
 	char **argv;
 	int argc;
 }command;
 
-void spawn_command(int in, int out, command *cmd){
+void spawn_command(int in, int out, struct command *cmd){
 	pid_t child_pid;
 
 	if((child_pid = fork()) < 0){
 		perror("fork() error \n");
 	}
 	
-	if (child_pid == 0 ){ // child process
+	if (child_pid == 0 ){
 
 		if (in != STDIN_FILENO){
 			// should be replaced with dup2; same for the following
@@ -41,6 +42,7 @@ void spawn_command(int in, int out, command *cmd){
 
 	}
 }
+
 int shell_execute(char ** args, int argc)
 {
 	if(strcmp(args[0], "EXIT") == 0)
@@ -48,47 +50,53 @@ int shell_execute(char ** args, int argc)
 	
 	int fd[2];
 
-	int input_fd = 0;
-	int output_fd = 1;
+	int input_fd = STDIN_FILENO;
+	int output_fd = STDOUT_FILENO;
 
-	int head = 0;
-	int tail = 0;
+	int head = 0;// the head of the command
+	int tail = 0;// the tail of the command
 
 	while(head < argc - 1)
 	{
-		while(tail < argc -1 && (args + tail)[0][0] != '|')
+		while(tail < argc -1 && strcmp(args[tail], "|") != 0)
 			tail++;
 		command *cmd = (command *)malloc(sizeof(command));
 		cmd->argc = tail - head;
-		cmd->argv = (char **)malloc(sizeof(char *) * (cmd->argc + 1));
+		cmd->argv = malloc(sizeof(char *) * (cmd->argc + 1));
 		for(int i = 0; i < cmd->argc; i++)
 			cmd->argv[i] = args[head + i];
 		cmd->argv[cmd->argc] = NULL;
 
-		if(tail < argc - 1) // not the last command
+		if(tail == argc - 1) //last command
 		{
+			output_fd = 1;//STDOUT_FILENO;
+			spawn_command(input_fd, output_fd, cmd);
+		}
+		else if(tail < argc - 1){ //not the last command
 			pipe(fd);
 			output_fd = fd[1];
 			spawn_command(input_fd, output_fd, cmd);
 
-			if(input_fd != 0)
+			if(input_fd != 0)//close the input file descriptor
 				close(input_fd);
-			if(output_fd != 1)
+			if(output_fd != 1)//close the output file descriptor
 				close(output_fd);
 			input_fd = fd[0];// the output of the previous command is the input of the next command
 		}
-		else if(tail == argc - 1){ // last command
-			output_fd = 1;//STDOUT_FILENO;
-			spawn_command(input_fd, output_fd, cmd);
-		}
-
+		
 		free(cmd);
-		pid_t status = 0;
-		while(wait(&status) != -1);// wait for all the child processes to finish
+
+		if (tail >= argc - 1) break;//last command
+		if ((args + tail)[0][0] == '|') head = tail = tail + 1;//skip the '|'
+			else break;
+	}
+
+		pid_t wpid;
+		int status = 0;
+		while ((wpid = wait(&status)) > 0); // wait for all the child processes 
 		
 		return 0;
 	}
 
 
 	
-}
